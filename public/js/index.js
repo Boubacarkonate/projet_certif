1347e4369b196452be253aef30b20a856ae9ffb6
Bouba <boubacar.konate@outlook.fr>
2023-04-01 19:28:17 +0200

    ajout dossier media et taf page home et base

---
+++ public/js/index.js
@@ -0,0 +1,36 @@
+// const items = document.querySelectorAll('img');
+// const nbSlide = items.length;
+// const suivant = document.querySelector('.right');
+// const precedent = document.querySelector('.left');
+// let count = 1;                                              //count = 0
+
+// function slideSuivante(){
+//     items[count].classList.remove('active');                                
+
+//     if(count < nbSlide - 1){
+//         count++;
+//     } else {
+//         count = 1;                                  //count = 0;
+//     }
+
+//     items[count].classList.add('active')
+//     console.log(count);
+    
+// }
+// suivant.addEventListener('click', slideSuivante)
+
+
+// function slidePrecedente(){
+//     items[count].classList.remove('active');
+
+//     if(count > 1){                                          //count > 0
+//         count--;                                                                             js du slide menu
+//     } else {
+//         count = nbSlide - 1;
+//     }
+
+//     items[count].classList.add('active')
+//      console.log(count);
+    
+// }
+// precedent.addEventListener('click', slidePrecedente)
